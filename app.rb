52c4944aa6b956a1ec5f50c90fca2fc1013b019e
Zach Feldman <zachfeldman@gmail.com>
2022-02-03 11:04:22 -0500

    JSON data storage, PUT updates, etc.

--- app.rb
+++ app.rb
@@ -1,9 +1,35 @@
 require 'sinatra'
+require 'json'
+require 'pry'
 
-APES = File.open('apes.txt').read.split("\n\n---\n\n").map do |ape|
-  {name: ape.split("\n")[0].split(": ")[1], photo: ape.split("\n")[1].split(": ")[1]}
+def read_apes
+  JSON.parse(File.open('apes.json').read)
 end
 
 get '/' do
   erb :ape_index
+end
+
+
+get '/apes/:ape_number' do
+  @ape = read_apes[params[:ape_number].to_i - 1]
+  erb :ape_show
+end
+
+put '/apes/:ape_number' do
+  ape_file = read_apes
+  ape_file[params[:ape_number].to_i - 1]['name'] = params[:name]
+  ape_file[params[:ape_number].to_i - 1]['photo'] = params[:photo]
+  File.write('apes.json', ape_file.to_json)
+  redirect '/'
+end
+
+post '/apes' do
+  ape_file = read_apes
+  ape_count = ape_file.length
+  ape_file[ape_count] = {}
+  ape_file[ape_count][:name] = params[:name]
+  ape_file[ape_count][:photo] = params[:photo]
+  File.write('apes.json', ape_file.to_json)
+  redirect '/'
 end
